512e3faef48ec2afb054dd08dee42b5ec916595d
oluobamzy <oluobamzy@gmail.com>
2023-08-14 13:10:38 -0400

    add hooks to file

---
+++ frontend/src/Hooks/reducer.js
@@ -0,0 +1,59 @@
+// reducer.js
+const initialState = {
+  favoritePhotoIds: [],
+  isModalOpen: false,
+  selectedPhoto: null,
+  photos:[],
+  topics:[],
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'TOGGLE_FAVORITE':
+      const { photoId } = action.payload;
+      if (state.favoritePhotoIds.includes(photoId)) {
+        return {
+          ...state,
+          favoritePhotoIds: state.favoritePhotoIds.filter(id => id !== photoId),
+        };
+      } else {
+        return {
+          ...state,
+          favoritePhotoIds: [...state.favoritePhotoIds, photoId],
+        };
+      }
+    case 'OPEN_MODAL':
+      return {
+        ...state,
+        isModalOpen: true,
+        selectedPhoto: action.payload.photo,
+      };
+    case 'CLOSE_MODAL':
+      return {
+        ...state,
+        isModalOpen: false,
+        selectedPhoto: null,
+      };
+      case 'fetchPhotos':
+        return {
+          ...state,
+          photos: action.payload.photos,
+        };
+        case 'fetchTopics':
+          return {
+            ...state,
+            topics: action.payload.topics,
+            selectedTopic: state.selectedTopic, // Keep the selected topic
+            photosForTopic: [], // Reset photos for the topic when fetching topics
+          };
+        case 'FETCH_PHOTOS_FOR_TOPIC':
+          return {
+            ...state,
+            photosForTopic: action.payload.photos,
+          };
+    default:
+      return state;
+  }
+};
+
+export { initialState, reducer };
---
+++ frontend/src/Hooks/useApplicationData.js
@@ -0,0 +1,116 @@
+import { useEffect, useReducer } from 'react';
+import { initialState, reducer } from './reducer';
+
+const useApplicationData = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    const fetchPhotos = () => {
+      fetch('/api/photos')
+        .then((response) => response.json())
+        .then((data) =>
+          dispatch({ type: 'fetchPhotos', payload: { photos: data } })
+        );
+    };
+    
+    const fetchTopics = () => {
+      fetch('/api/topics')
+        .then((response) => response.json())
+        .then((data) => {
+          dispatch({ type: 'fetchTopics', payload: { topics: data } });
+    
+          // Fetch photos for the initially selected topic (if available)
+          if (state.selectedTopic) {
+            fetchPhotosForTopic(state.selectedTopic);
+          }
+        });
+    };
+    fetchPhotos();
+    fetchTopics();
+  }, []);
+  
+  const toggleFavorite = (photoId) => {
+    dispatch({ type: 'TOGGLE_FAVORITE', payload: { photoId } });
+  };
+
+  const clickTopic = (topicSlug) => {
+    dispatch({ type: 'CLICK_TOPIC', payload: { topicSlug } });
+  };
+
+  const handleModalOpen = (photo) => {
+    dispatch({ type: 'OPEN_MODAL', payload: { photo } });
+  };
+
+  const handleModalClose = () => {
+    dispatch({ type: 'CLOSE_MODAL' });
+  };
+
+  const { photos, topics } = state;
+
+  const topicArr = topics.map((topic) => ({
+    id: topic.id,
+    slug: topic.slug,
+    title: topic.title,
+    onClick: () => clickTopic(topic.slug),
+  }));
+
+  const photosArr = photos.map((photo) => ({
+      id: photo.id,
+      location: {
+        city: photo.location.city,
+        country: photo.location.country,
+      },
+      imageSource: photo.urls.regular,
+      username: photo.user.name,
+      profile: photo.user.profile,
+      similar_photos: photo.similar_photos,
+      likes: 0,
+      dislikes: 0,
+      favorite: state.favoritePhotoIds.includes(photo.id),
+    }));
+
+  const similarPhotosArr = photos.map((photo) => ({
+    id: photo.id,
+    location: {
+      city: photo.location.city,
+      country: photo.location.country,
+    },
+    imageSource: photo.urls.full,
+    username: photo.user.name,
+    profile: photo.user.profile,
+    s_photos: photo.similar_photos,
+    likes: 0, // Initialize likes to 0
+    dislikes: 0,
+  }));
+  const fetchPhotosForTopic = (topicId) => {
+    fetch(`/api/topics/photos/${topicId}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Fetch error: ${response.status} - ${response.statusText}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        dispatch({ type: 'FETCH_PHOTOS_FOR_TOPIC', payload: { photos: data } });
+      })
+      .catch((error) => {
+        console.error('Error fetching photos:', error);
+      });
+  };
+    useEffect(() => {
+    if (topicArr.length > 0) {
+      fetchPhotosForTopic(topicArr[0].slug); // Fetch photos for the first topic initially
+    }
+  }, [topicArr]);
+  return {
+    ...state,
+    toggleFavorite,
+    topicArr,
+    similarPhotosArr,
+    photosArr,
+    handleModalOpen,
+    handleModalClose,
+  };
+};
+
+export { useApplicationData };
