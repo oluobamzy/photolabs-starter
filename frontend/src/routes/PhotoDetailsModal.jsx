b0659edd5b80196233de253f299069a27ef619a4
oluobamzy <oluobamzy@gmail.com>
2023-08-14 17:37:33 -0400

    updating modal stylings

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -14,15 +14,12 @@ const PhotoDetailsModal = ({ isOpen, onClose, imageSource, photos, toggleFavorit
       <button className="photo-details-modal__close-button photo-details-modal__top-bar" onClick={onClose}>
         <img src={closeSymbol} alt="close symbol" />
       </button>
-      <div className="photo-details-modal__images">
+      <div className="photo-details-modal__images photo-details-modal__image ">
          <img src={imageSource.image} alt="photo details" />
       </div>
-       <div>
-        <h2 className='photo-details-modal__header'>Similar Photos</h2>
+        <h2>Similar Photos</h2>
         <div className='photo-details-modal__images'>
         <PhotoList photos={photos} toggleFavorite={toggleFavorite}favoritePhotoIds={favoritePhotoIds}/>
-        </div>
-        
        </div>
     </div>
   )
